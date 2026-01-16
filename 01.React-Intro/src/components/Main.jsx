5f9cbbe25c44b50f47525f9b979f798cea1e1d9e
fadilakarimli <fadilafk@code.edu.az>
2026-01-16 12:02:41 +0400

    added

---
+++ 01.React-Intro/src/components/Main.jsx
@@ -0,0 +1,12 @@
+
+function Main() {
+  return (
+    <>
+      <h2>Welcome!</h2>
+      <p>Component nədir? — React-də UI elementlərini bölmələrə ayırmaq üçün istifadə olunur.</p>
+      <p>JSX nədir? — JavaScript daxilində HTML oxşar sintaksis ilə elementləri yazmağa imkan verən syntax extension-dır.</p>
+    </>
+  );
+}
+
+export default Main;
---
+++ 01.React-Intro/src/components/Welcome.jsx
@@ -0,0 +1,8 @@
+
+function Welcome({ user }) {
+  return (
+    <h2>Welcome, {user}!</h2>
+  );
+}
+
+export default Welcome;
